b5e7c2193551dacfcc54c4497ba58b126501acd0
Ponneelan <ponneelan1305@gmail.com>
2023-04-19 19:11:42 +0530

    JavaScripting practiced files added

---
+++ objects-keys.js
@@ -0,0 +1,8 @@
+const car = {  
+       make: 'Honda',  
+       model: 'Accord',  
+       year: 2020  
+     }
+
+let obj = Object.keys(car);
+console.log(obj)
---
+++ stringRevising.js
@@ -0,0 +1,3 @@
+let pizza = 'pizza is awesome'
+pizza = pizza.replace('awesome','wonderful')
+console.log(pizza);
